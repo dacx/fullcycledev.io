f9cb60fdfa4c7739dac150d3469ee73b3c9174e1
Michael Brayer <michael@brayer.dev>
2023-11-21 21:18:00 +0100

    #5 adds hero section

--- fullcycledev/socials/views.py
+++ fullcycledev/socials/views.py
@@ -9,7 +9,7 @@ from fullcycledev.users.models import User
 def home(request: HttpRequest):
     users_count: int = User.objects.count()
     if SocialStats.objects.count() < 2:
-        return render(request, "pages/home.html", {"users_count": users_count})
+        return render(request, "home/home.html", {"users_count": users_count})
     stats: QuerySet[SocialStats] = SocialStats.objects.all()[:2]
     current: SocialStats = stats[0]
     delta: dict = {
@@ -19,4 +19,4 @@ def home(request: HttpRequest):
         "discord_members": current.discord_members - stats[1].discord_members,
     }
 
-    return render(request, "pages/home.html", {"current": current, "delta": delta, "users_count": users_count})
+    return render(request, "home/home.html", {"current": current, "delta": delta, "users_count": users_count})
